afbc445d46db5df11c2807873341d37188b05e3b
Osioke Itseuwa <osiokeitseuwa@gmail.com>
2026-03-05 21:13:00 +0100

    bump cache_name to syd-v13

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'syd-v12';
+const CACHE_NAME = 'syd-v13';
 
 // These are the app shell files we want to pre-cache during install.
 // On every new deploy, bump CACHE_NAME so the install event fires again,
